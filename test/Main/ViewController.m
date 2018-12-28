6cd5129258ebec5a11df0c85f49326fffd8e8690
wuzhenli <zhenli@6.cn>
2018-12-28 11:03:37 +0800

    1、测试宏 __VA_ARGS__

--- test/Main/ViewController.m
+++ test/Main/ViewController.m
@@ -36,7 +36,7 @@ dispatch_async(dispatch_get_main_queue(), block); \
                              @"FunctionTestViewController",@"StaticTableViewController",
                              @"SwipeCellKitCollectionViewController",
                              @"TestAnimationViewController", @"DrawViewController",
-                             @"GestureViewController", @"DefinePanGestureViewController", @"PhotoKitViewController", @"SlideTableViewController", @"KeyboardViewController", @"WebViewController", @"ScrollViewController", @"TestVCController", @"MenuCtlViewController"].mutableCopy;
+                             @"GestureViewController", @"DefinePanGestureViewController", @"PhotoKitViewController", @"SlideTableViewController", @"KeyboardViewController", @"WebViewController", @"ScrollViewController", @"TestVCController", @"MenuCtlViewController", @"TestViewController"].mutableCopy;
     }
     return _arrClassNameXib;
 }
--- test/Sections/test/TestViewController.m
+++ test/Sections/test/TestViewController.m
@@ -8,6 +8,10 @@
 
 #import "TestViewController.h"
 
+#define TEST_ARGS(...)  NSLog(@"--- before"); \
+__VA_ARGS__ \
+NSLog(@"--- after");
+
 @interface TestViewController ()
 
 @end
@@ -17,38 +21,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    UIWindow *window = [self getFrontWindow];
     
-    static CGFloat offset = 0;
-    UIView *viewRed = ({
-        UIView *v = [[UIView alloc] initWithFrame:CGRectMake(100 + offset, 100 + offset, 100, 100)];
-        offset += 3;
-        v.backgroundColor = RandomColor;
-        v;
-    });
-    
-    [window addSubview:viewRed];
-    
+    TEST_ARGS(NSLog(@"anything"););
 }
 
-- (UIWindow *)getFrontWindow {
-    NSEnumerator<__kindof UIWindow *> *frontToBackWindows = [UIApplication sharedApplication].windows.reverseObjectEnumerator;
- 
-    for (UIWindow *win in frontToBackWindows) {
-        BOOL visible = win.alpha > 0 && !win.hidden;
-        BOOL onMainScreen = win.screen == [UIScreen mainScreen];
-        BOOL isKeyWindow = win.isKeyWindow;
-        
-        if (visible && onMainScreen && isKeyWindow) {
-            NSLog(@"win:%@", win);
-            return win;
-        }
-    }
-    
-    return nil;
-}
 
 @end
