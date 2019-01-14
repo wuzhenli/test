9ae83ab727e02b4a6a4f135bbf26c66963e946f4
wuzhenli <zhenli@6.cn>
2019-01-14 13:25:14 +0800

    add gitignore

--- build/test.build/Release-iphonesimulator/test.build/Objects-normal/x86_64/MGSwipeTableCell.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Verders/MGSwipeTableCell/MGSwipeTableCell.m \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Others/PrefixHeader.pch \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Macro/TestHeader.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Macro/Global.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Macro/LogMacro.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Verders/MGSwipeTableCell/MGSwipeTableCell.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Verders/MGSwipeTableCell/MGSwipeButton.h
--- build/test.build/Release-iphonesimulator/test.build/Objects-normal/x86_64/NSSafeMuDictionary.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Models/NSSafeMuDictionary.m \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Others/PrefixHeader.pch \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Macro/TestHeader.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Macro/Global.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Macro/LogMacro.h \
-  /Users/li/PrivateText/GitHub-Demo/test/test/Models/NSSafeMuDictionary.h
